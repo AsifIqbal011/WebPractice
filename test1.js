eb902f5a5afaf0ae7b05539e4f816e71aa5a598a
Asif Iqbal <asifiqbalnewac@gmail.com>
2025-06-28 18:48:55 +0600

    till async await

--- test1.js
+++ test1.js
@@ -1,4 +1,4 @@
-/* 
+ 
 var name = "Asif";    // older way
 let age = 25;         // modern
 const country = "BD"; // cannot be reassigned
@@ -6,6 +6,7 @@ const country = "BD"; // cannot be reassigned
 console.log(name);
 console.log(age);
 console.log(country);
+console.log(`Hello ${name}, How are you?`)
 
 for (let i = 0; i < 5; i++) {
   console.log(i);
@@ -17,19 +18,43 @@ function greet(name) {
 }
 console.log(greet("Asif"))
 
+//arrow function
 let greet1 = (name) => {console.log("hello", name)}
 greet1("samad")
-*/
 
+//destruction
 const numbers = [1, 2, 3];
 const [a, b] = numbers;     //a=1, b=2
 console.log(b)
 
 const person = { name1: "Asif", age1: 25 };
 const { name1, age1 } = person;
-
 console.log(age1);
 
+//spread operator
 const arr1 = [1, 2];
 const arr2 = [...arr1, 3, 4]; 
 console.log(arr2);
+
+
+//json
+const user = { name: "Asif", age: 25 };
+const jsonStr = JSON.stringify(user); // convert to JSON string
+const parsed = JSON.parse(jsonStr);   // back to object
+
+console.log(jsonStr);
+console.log(parsed);
+
+//async await
+async function fetchUser() {
+    try {
+        const res= await fetch('https://jsonplaceholder.typicode.com/todos/1');
+        const data = await res.json();
+        console.log(data)
+    } catch (error) {
+        console.log(error);
+    }
+}
+fetchUser();
+console.log("I am last line");
+
