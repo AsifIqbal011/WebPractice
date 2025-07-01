bebb2d8ea65856c12fbe53b2ae4998304019af48
Asif Iqbal <asifiqbalnewac@gmail.com>
2025-07-01 22:57:13 +0600

    Dom added

--- test1.js
+++ test1.js
@@ -1,84 +1,93 @@
-import promptSync from 'prompt-sync';
-const prompt = promptSync();
+// import promptSync from 'prompt-sync';
+// const prompt = promptSync();
 
-var name = "Asif";    // older way
-let age = 25;         // modern
-const country = "BD"; // cannot be reassigned
+// var name = "Asif";    // older way
+// let age = 25;         // modern
+// const country = "BD"; // cannot be reassigned
 
-console.log(name);
-console.log(age);
-console.log(country);
-console.log(`Hello ${name}, How are you?`)
+// console.log(name);
+// console.log(age);
+// console.log(country);
+// console.log(`Hello ${name}, How are you?`)
 
-for (let i = 0; i < 5; i++) {
-  console.log(i);
-} 
-
-
-function greet(name) {
-  return `Hello, ${name}`;
-}
-console.log(greet("Asif"))
-
-//arrow function
-let greet1 = (name) => {console.log("hello", name)}
-greet1("samad")
-
-//destruction
-const numbers = [1, 2, 3];
-const [a, b] = numbers;     //a=1, b=2
-console.log(b)
-
-const person = { name1: "Asif", age1: 25 };
-const { name1, age1 } = person;
-console.log(age1);
-
-//spread operator
-const arr1 = [1, 2];
-const arr2 = [...arr1, 3, 4]; 
-console.log(arr2);
-
-//rest operator
-function sum(...numbers) {                    //reduce terates over the array elements in ascending-index order, accumulating them into a single value. 
-  return numbers.reduce((a, b) => a - b, 0);  //0 + 5 = 5
-}                                             //5 + 10 = 15
-console.log(sum(5, 10, 20));                 //15 + 20 = 35
+// for (let i = 0; i < 5; i++) {
+//   console.log(i);
+// } 
 
 
-//json
-const user = { name: "Asif", age: 25 };
-const jsonStr = JSON.stringify(user); // convert to JSON string
-const parsed = JSON.parse(jsonStr);   // back to object
+// function greet(name) {
+//   return `Hello, ${name}`;
+// }
+// console.log(greet("Asif"))
 
-console.log(jsonStr);
-console.log(parsed);
+// //arrow function
+// let greet1 = (name) => {console.log("hello", name)}
+// greet1("samad")
 
-//async await
-async function fetchUser() {
-    try {
-        const res= await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        const data = await res.json();
-        console.log(data)
-    } catch (error) {
-        console.log(error);
-    }
-}
-fetchUser();
-console.log("I am last line");
+// //destruction
+// const numbers = [1, 2, 3];
+// const [a, b] = numbers;     //a=1, b=2
+// console.log(b)
 
-//module
-import { add } from './export.js';
-console.log(add(5,7))
+// const person = { name1: "Asif", age1: 25 };
+// const { name1, age1 } = person;
+// console.log(age1);
 
-function BMICalculator(w,h){
-  let bmi=w/(h*h);
-  return bmi;
-}
+// //spread operator
+// const arr1 = [1, 2];
+// const arr2 = [...arr1, 3, 4]; 
+// console.log(arr2);
 
+// //rest operator
+// function sum(...numbers) {                    //reduce terates over the array elements in ascending-index order, accumulating them into a single value. 
+//   return numbers.reduce((a, b) => a - b, 0);  //0 + 5 = 5
+// }                                             //5 + 10 = 15
+// console.log(sum(5, 10, 20));                 //15 + 20 = 35
 
 
-let weight= prompt("Enter your weight in kg: ");
-let height= prompt("Enter your height in metre: ");
+// //json
+// const user = { name: "Asif", age: 25 };
+// const jsonStr = JSON.stringify(user); // convert to JSON string
+// const parsed = JSON.parse(jsonStr);   // back to object
 
-let BMI=BMICalculator(weight,height);
-console.log(BMI.toFixed(4));
+// console.log(jsonStr);
+// console.log(parsed);
+
+// //async await
+// async function fetchUser() {
+//     try {
+//         const res= await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//         const data = await res.json();
+//         console.log(data)
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
+// fetchUser();
+// console.log("I am last line");
+
+// //module
+// import { add } from './export.js';
+// console.log(add(5,7))
+
+// function BMICalculator(w,h){
+//   let bmi=w/(h*h);
+//   return bmi;
+// }
+
+
+
+// let weight= prompt("Enter your weight in kg: ");
+// let height= prompt("Enter your height in metre: ");
+
+// let BMI=BMICalculator(weight,height);
+// console.log(BMI.toFixed(4));
+
+
+var lastitem= document.firstElementChild.lastElementChild.getElementsByClassName('list')[2];
+lastitem.innerHTML="Asif";
+
+//console.log(document.firstElementChild.lastElementChild.querySelector('ul').lastElementChild.innerHTML="Asif");
+
+document.querySelector('ul .list a').style.color='red'
+console.log(document.querySelectorAll('ul .list')[1].innerHTML)
